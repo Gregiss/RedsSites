481cf58332abc333ba57de5ee76419a9341c3549
Alexandre Silva <xande1231231@hotmail.com>
2020-08-06 16:19:57 -0300

    New site

---
+++ precache-manifest.e23f18636204dbd74c337ba2b7df79d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ba8a8f3c6264586d9b60a7ad8af06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13757ef7032bf9d1409a",
+    "url": "/static/css/main.86e22e1a.chunk.css"
+  },
+  {
+    "revision": "aa663aa75f57642fb47a",
+    "url": "/static/js/2.b3f63666.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b3f63666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13757ef7032bf9d1409a",
+    "url": "/static/js/main.c9c330b7.chunk.js"
+  },
+  {
+    "revision": "3ef33fd9ac34ed507089",
+    "url": "/static/js/runtime-main.6e136cfa.js"
+  },
+  {
+    "revision": "265d6a7a505a303087bc598555442b9a",
+    "url": "/static/media/back.265d6a7a.jpg"
+  },
+  {
+    "revision": "429fb79287e61fcf15d5d8cce3f76596",
+    "url": "/static/media/steam_icon.429fb792.png"
+  }
+]);
