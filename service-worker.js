7c49274a56761cd08201eca70aed5cca3a5cf065
Hurricane404 <46573030+Hurricane404@users.noreply.github.com>
2020-08-07 14:49:53 -0300

    Fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72a4b150488bad01f841d76071ca8ce1.js"
+  "/precache-manifest.9ab06edbf565465cd480db15e8fa1f09.js"
 );
 
 self.addEventListener('message', (event) => {
